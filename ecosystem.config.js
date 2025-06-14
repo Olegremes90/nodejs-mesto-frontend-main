bfdea0d5c4fd01be44b73d4813fe629ede107d77
Olegremes90 <113620810+Olegremes90@users.noreply.github.com>
2025-06-14 14:57:53 +0300

    внес корректировки

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,21 +1,27 @@
 require('dotenv').config({ path: '.env.front' });
 
 const {
-    DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF = 'origin/master',
+    DEPLOY_USER,
+    DEPLOY_HOST,
+    DEPLOY_PATH,
+    DEPLOY_REF = 'origin/master',
 } = process.env;
 
 module.exports = {
-    apps: [{
-        name: 'frontend',
-        script: 'serve',  // Используем serve для раздачи статических файлов
-        cwd: DEPLOY_PATH,  // Указываем директорию, где будет папка build
-        env: {
-            NODE_ENV: 'production',
-            PORT: 3000
+    apps: [
+        {
+            name: 'frontend',
+            script: 'serve',
+            args: ['-s', 'build', '-l', '3000'], // раздача статических файлов из build на порту 3000
+            cwd: DEPLOY_PATH,                    // директория проекта на сервере (где есть папка build)
+            env: {
+                NODE_ENV: 'production',
+                PORT: 3000,
+            },
+            watch: false
         }
-    }],
+    ],
 
-    // Настройка деплоя
     deploy: {
         production: {
             user: DEPLOY_USER,
@@ -23,8 +29,9 @@ module.exports = {
             ref: DEPLOY_REF,
             repo: 'https://github.com/Olegremes90/nodejs-mesto-frontend-main.git',
             path: DEPLOY_PATH,
-            'pre-deploy-local': `scp .env.front ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
-            'post-deploy': 'export NODE_OPTIONS=--openssl-legacy-provider && source ~/.nvm/nvm.sh && npm install && npm run build && pm2 startOrReload ecosystem.config.js --only frontend',  // Используем startOrReload
+            'pre-deploy-local': `scp .env.front ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/.env`,
+            'post-deploy': 'export NODE_OPTIONS=--openssl-legacy-provider && source ~/.nvm/nvm.sh && npm install && npm run build && pm2 startOrReload ecosystem.config.js --only frontend',
+            ssh_options: 'StrictHostKeyChecking=no',
         },
     },
 };
