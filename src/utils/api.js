7f0874099153ed41ae185e1d4554ff4569a6e2ce
Olegremes90 <113620810+Olegremes90@users.noreply.github.com>
2025-06-15 13:20:23 +0300

    подправил конфиг

--- src/utils/api.js
+++ src/utils/api.js
@@ -139,6 +139,6 @@ class Api {
   }
   }
   // Замените на адрес вашего бэкенда
-  const api = new Api('https://mesto.students.nomorepartiessbs.ru');
+  const api = new Api('https://api.mesto.students.nomorepartiessbs.ru');
 
   export default api;
